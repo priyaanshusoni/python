f9430bd5f37d23fe38e42843b8e6e8661f94251b
Priyanshu Soni <priyanshusoni0927@gmail.com>
2026-04-13 16:52:59 +0530

    gen-ai folfer created

---
+++ gen-ai/first.py
@@ -0,0 +1,9 @@
+from google import genai
+
+
+client = genai.Client(api_key = 'YOUR_API_KEY')
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+)
+print(response.text)
